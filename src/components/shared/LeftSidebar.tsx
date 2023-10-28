c0d4fe5daa9477ca684663a669c8a5cd4698a1ff
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2023-10-29 00:03:10 +0530

    Posts displaying with implemented like and save

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -24,7 +24,7 @@ const LeftSidebar = () => {
                     <img src='/assets/images/logo.svg' alt='logo' width={170} height={36} />
                 </Link>
                 <Link to={`/profile/${user.id}`} className='flex gap-3 items-center'>
-                    <img src={user.imageUrl || '/assets/icons/profile-placeholder.jpg'} alt="profile" className='h-14 w-14 rounded-full' />
+                    <img src={user.imageUrl || '/assets/icons/profile-placeholder.svg'} alt="profile" className='h-14 w-14 rounded-full' />
                     <div className='flex flex-col'>
                         <p className='body-bold'>{user.name}</p>
                         <p className='small-regular text-light-3'>@{user.username}</p>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,51 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export function formatDateString(dateString: string) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString("en-US", options);
+
+  const time = date.toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+  });
+
+  return `${formattedDate} at ${time}`;
+}
+
+export function formatRelativeDate(dateString: string): string {
+  const now = new Date();
+  const date = new Date(dateString);
+
+  const timeDifference = now.getTime() - date.getTime();
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (days > 0) {
+    return `${days} day${days > 1 ? "s" : ""} ago`;
+  } else if (hours > 0) {
+    return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+  } else if (minutes > 0) {
+    return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
+  } else {
+    return "Just now";
+  }
+}
+
+export const checkIsLiked = (likeList: string[], userId: string) => {
+  return likeList.includes(userId);
+};
